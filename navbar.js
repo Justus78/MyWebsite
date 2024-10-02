04561f8285514347c6b04f41e401d06fc641c058
Matt Justus <stars787878@aol.com>
2024-10-02 09:28:17 -0400

    added new folder for components, addeds footer to components, modifed magicapp.html bootstrap classes.

--- navbar.js
+++ navbar.js
@@ -1,6 +1,13 @@
-fetch('/navbar.html')
+fetch('/Components/navbar.html')
         .then(response => response.text())
         .then(html => {
             document.getElementById('navbar').innerHTML = html;
         })
         .catch(error => console.error('Error loading the navbar:', error));
+
+fetch('/Components/footer.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('footer').innerHTML = html;
+        })
+        .catch(error => console.error('error loading the footer:', error));
